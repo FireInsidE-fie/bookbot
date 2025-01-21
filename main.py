abe7b05105345fd6ae34d79dcf36db1c6d771730
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-01-21 12:21:45 +0100

    add print_report function

--- main.py
+++ main.py
@@ -16,24 +16,28 @@ def character_count(text):
                 characters[char] = 1
     return characters
 
+def print_report(path, characters, total_words):
+    print(f"[!] - Beginning report for {path}...")
+    print(f"[!] - Total words: {total_words}")
+    for char in characters:
+        if char.isalpha():
+            print(f"Number of '{char}' in text: {characters[char]}")
+
 def main():
     path = "books/frankenstein.txt"
     total_words = 0
 
     print("[!] - Beginning execution...")
-    print(f"[!] - Printing contents of {path}")
+    # print(f"[!] - Printing contents of {path}")
 
     with open(path) as f:
         characters = character_count(f)
     with open(path) as f: # Should probably find a way to do the word counting at the same time as char counting
         for line in f:
-            print(line.strip())
+            # print(line.strip())
             total_words += word_count(line.strip())
 
-    print(f"[?] - Input text contained {total_words} words")
-    print(f"[?] - Now printing dictionary of characters contained...")
-
-    print(characters)
+    print_report(path, characters, total_words)
 
     print("[!] - Exiting...")
 

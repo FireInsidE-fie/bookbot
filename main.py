999297a1cf3fc92fd3a3d3780a959b06c0efd9de
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-01-21 11:32:24 +0100

    add character count function

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def word_count(text):
         count += 1
     return count
 
+def character_count(text):
+    characters = {}
+    for line in text:
+        for char in line:
+            char = char.lower()
+            if char in characters:
+                characters[char.lower()] += 1
+            else:
+                characters[char] = 1
+    return characters
+
 def main():
     path = "books/frankenstein.txt"
     total_words = 0
@@ -13,11 +24,16 @@ def main():
     print(f"[!] - Printing contents of {path}")
 
     with open(path) as f:
+        characters = character_count(f)
+    with open(path) as f: # Should probably find a way to do the word counting at the same time as char counting
         for line in f:
             print(line.strip())
             total_words += word_count(line.strip())
 
     print(f"[?] - Input text contained {total_words} words")
+    print(f"[?] - Now printing dictionary of characters contained...")
+
+    print(characters)
 
     print("[!] - Exiting...")
 
